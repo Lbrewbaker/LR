7aaee795222fbb4ac05a24a92b3bac8365f10319
Luke Brewbaker <8764139+Lbrewbaker@users.noreply.github.com>
2018-05-02 16:42:42 -0600

    stop/start powershell script - usefull on XMs

---
+++ stopStart.ps1
@@ -0,0 +1,26 @@
+﻿Do{
+$input = Read-Host -Prompt '1:Start or 2:Stop services?'
+
+
+#starts services and exits script
+If ($input -eq 1){
+Write-Host "Starting Services"
+Get-Service | Where-Object {$_.displayName.StartsWith("LogRhythm")} | Start-Service
+break; 
+}
+
+
+ #stops services and exits script
+ElseIf ($input -eq 2){
+Write-Host "Stopping Services"
+Get-Service | Where-Object {$_.displayName.StartsWith("LogRhythm")} | Stop-Service
+break;
+}
+
+Else{
+Write-Host "Invalid Input"
+}
+
+}
+While ($true)
+
